8554c018c51c8ac75398f2a0fae5ad16c34557a2
Cupid Zhang <740706762@qq.com>
2017-05-24 14:48:14 +0800

    java_spider

---
+++ 源码/LaoNianBao_zp/SpliderDemo/src/com/zhy/spider/rule/RuleException.java
@@ -0,0 +1,30 @@
+package com.zhy.spider.rule;
+
+public class RuleException extends RuntimeException
+{
+
+	public RuleException()
+	{
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public RuleException(String message, Throwable cause)
+	{
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RuleException(String message)
+	{
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RuleException(Throwable cause)
+	{
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
